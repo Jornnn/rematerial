ec90accb22305da095ba7410a0fe7ffb7ba2aa35
ultraJorn <1056355@hr.nl>
2026-01-06 11:17:29 +0100

    vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173,
+    allowedHosts: ['rematerial-frontend.onrender.com'],
     watch: {
       usePolling: true
     }
